15507d32fd70597c270ff7feadad2aff725eb389
QURARE <75406469+QURARE@users.noreply.github.com>
2021-04-05 15:10:31 +0900

    배경 스크롤링

---
+++ Assets/Scripts/BackgroundLoop.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundLoop : MonoBehaviour
+{
+    private float width;
+
+    private void Awake()
+    {
+        BoxCollider2D backgroundCollider = GetComponent<BoxCollider2D>();
+        width = backgroundCollider.size.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
