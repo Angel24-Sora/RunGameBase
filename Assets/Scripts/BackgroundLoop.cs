96b98da709ee8ee1999056b4aa9764af630dd80f
QURARE <75406469+QURARE@users.noreply.github.com>
2021-04-12 15:07:39 +0900

    0412jkh

--- Assets/Scripts/BackgroundLoop.cs
+++ Assets/Scripts/BackgroundLoop.cs
@@ -15,6 +15,15 @@ public class BackgroundLoop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(transform.position.x <= -width)
+        {
+            Reposition();
+        }
+    }
+
+    private void Reposition()
+    {
+        Vector2 offset = new Vector2(width * 2f, 0);
+        transform.position = (Vector2)transform.position + offset;
     }
 }
